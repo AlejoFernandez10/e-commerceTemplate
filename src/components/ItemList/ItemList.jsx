8511257a9f4d61e5981ffb756506de9c3ae9acad
AlejoFernandez10 <alejofernandez1239@gmail.com>
2023-04-03 14:29:11 -0300

    Cambios de altura en slider de inicio

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -12,11 +12,11 @@ const ItemList = ({ prods }) => {
         return (
             <div >
                  <div className="w-100% flex flex-col justify-end items-end " >
-              <div className=" mx-auto max-w-2xl py-16 px-4  sm:px-6 lg:max-w-7xl lg:px-8 " >
+              <div className=" mx-auto max-w-2xl py-16 px-4  sm:px-6 md:max-w-4xl lg:max-w-7xl lg:px-8 " >
                 
 
               <div className="catalogo-container"></div>
-                <div className="  grid grid-cols-1 place-items-center gap-y-20 gap-x-4 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 xl:gap-x-20  ">
+                <div className="  grid grid-cols-1 place-items-center gap-y-14 gap-x-4 sm:grid-cols-2  md:grid-cols-3 lg:grid-cols-4 lg:gap-x-20  ">
                      { prods.map((prod)=>(
                         <Item 
                         id={prod.id}
